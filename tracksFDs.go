36081dbfdaee3e5787c7b1bec6e76606d5493ea3
dmoore91 <danmoore2706@gmail.com>
2020-11-01 00:57:01 -0400

    Finish implementing for 2

--- tracksFDs.go
+++ tracksFDs.go
@@ -782,169 +782,590 @@ func checkGroupsOfOneTrack(data []track) [27][27]bool {
 	return relArr
 }
 
-//func checkGroupOfTwo(data []user, relArr [6][6]bool) {
-//
-//	// Iterate through each column in db
-//	for groupOne := 0; groupOne < 6; groupOne++ {
-//
-//		for groupTwo := 0; groupTwo < 6; groupTwo++ {
-//
-//			if groupOne != groupTwo {
-//
-//				// All default to being valid functional dependencies. Change to false once we discover they are not
-//				// Reset after each group
-//				isValid := []bool{true, true, true, true, true, true}
-//
-//				// Need to create new maps for each group too or else we'll get collisions
-//				maps := genericMaps{
-//					Id:           make(map[string]int),
-//					Username:     make(map[string]sql.NullString),
-//					Kind:         make(map[string]sql.NullString),
-//					LastModified: make(map[string]sql.NullString),
-//					Permalink:    make(map[string]sql.NullString),
-//					Uri:          make(map[string]sql.NullString),
-//				}
-//
-//				// Iterate through each row in db
-//				for _, elem := range data {
-//
-//					key := getValueByColumnNum(groupOne, elem)
-//					key += getValueByColumnNum(groupTwo, elem)
-//
-//					// id
-//					id, ok := maps.Id[key]
-//
-//					if !ok {
-//						maps.Id[key] = elem.Id
-//					} else {
-//						// Since they differ, this is not a valid functional dependency
-//						if elem.Id != id {
-//
-//							isValid[0] = false
-//						}
-//					}
-//
-//					// username
-//					username, ok := maps.Username[key]
-//
-//					if !ok {
-//						maps.Username[key] = elem.Username
-//					} else {
-//						// Since they differ, this is not a valid functional dependency
-//						if elem.Username.String != username.String {
-//
-//							isValid[1] = false
-//						}
-//					}
-//
-//					// kind
-//					kind, ok := maps.Kind[key]
-//
-//					if !ok {
-//						maps.Kind[key] = elem.Kind
-//					} else {
-//						// Since they differ, this is not a valid functional dependency
-//						if elem.Kind.String != kind.String {
-//
-//							isValid[2] = false
-//						}
-//					}
-//
-//					// lastModified
-//					lastModified, ok := maps.LastModified[key]
-//
-//					if !ok {
-//						maps.LastModified[key] = elem.LastModified
-//					} else {
-//						// Since they differ, this is not a valid functional dependency
-//						if elem.LastModified.String != lastModified.String {
-//
-//							isValid[3] = false
-//						}
-//					}
-//
-//					// permalink
-//					permalink, ok := maps.Permalink[key]
-//
-//					if !ok {
-//						maps.Permalink[key] = elem.Permalink
-//					} else {
-//						// Since they differ, this is not a valid functional dependency
-//						if elem.Permalink.String != permalink.String {
-//
-//							isValid[4] = false
-//						}
-//					}
-//
-//					// uri
-//					uri, ok := maps.Uri[key]
-//
-//					if !ok {
-//						maps.Uri[key] = elem.Uri
-//					} else {
-//						// Since they differ, this is not a valid functional dependency
-//						if elem.Uri.String != uri.String {
-//
-//							isValid[5] = false
-//						}
-//					}
-//				}
-//
-//				header := ""
-//
-//				switch groupOne {
-//				case 0:
-//					header += "id,"
-//				case 1:
-//					header += "username,"
-//				case 2:
-//					header += "kind,"
-//				case 3:
-//					header += "last_modified,"
-//				case 4:
-//					header += "permalink,"
-//				case 5:
-//					header += "uri,"
-//				}
-//
-//				switch groupTwo {
-//				case 0:
-//					header += "id->"
-//				case 1:
-//					header += "username->"
-//				case 2:
-//					header += "kind->"
-//				case 3:
-//					header += "last_modified->"
-//				case 4:
-//					header += "permalink->"
-//				case 5:
-//					header += "uri->"
-//				}
-//
-//				for idx, valid := range isValid {
-//					if valid && idx != groupOne && idx != groupTwo &&
-//						!(relArr[groupOne][idx] || relArr[groupTwo][idx]) {
-//						switch idx {
-//						case 0:
-//							println(header + "id")
-//						case 1:
-//							println(header + "username")
-//						case 2:
-//							println(header + "kind")
-//						case 3:
-//							println(header + "last_modified")
-//						case 4:
-//							println(header + "permalink")
-//						case 5:
-//							println(header + "uri")
-//						}
-//					}
-//				}
-//			}
-//		}
-//	}
-//}
+func checkGroupOfTwoTrack(data []track, relArr [27][27]bool) {
+
+	// Iterate through each column in db
+	for groupOne := 0; groupOne < 27; groupOne++ {
+
+		for groupTwo := 0; groupTwo < 27; groupTwo++ {
+
+			if groupOne != groupTwo {
+
+				// All default to being valid functional dependencies. Change to false once we discover they are not
+				// Reset after each group
+				isValid := []bool{true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,
+					true, true, true, true, true, true, true, true, true, true, true}
+
+				// Need to create new maps for each group too or else we'll get collisions
+				maps := genericTrackMaps{
+					Id:                  make(map[string]int),
+					Title:               make(map[string]sql.NullString),
+					Uri:                 make(map[string]sql.NullString),
+					Isrc:                make(map[string]sql.NullString),
+					Genre:               make(map[string]sql.NullInt32),
+					Kind:                make(map[string]sql.NullInt32),
+					License:             make(map[string]sql.NullInt32),
+					LikesCount:          make(map[string]sql.NullInt32),
+					Commentable:         make(map[string]sql.NullBool),
+					CommentCount:        make(map[string]sql.NullInt32),
+					Downloadable:        make(map[string]sql.NullBool),
+					DownloadCount:       make(map[string]sql.NullInt32),
+					CreatedAt:           make(map[string]sql.NullString),
+					Description:         make(map[string]sql.NullString),
+					Duration:            make(map[string]sql.NullInt32),
+					LableName:           make(map[string]sql.NullString),
+					LastModified:        make(map[string]sql.NullString),
+					OriginalContentSize: make(map[string]sql.NullInt32),
+					OriginalFormat:      make(map[string]sql.NullString),
+					Permalink:           make(map[string]sql.NullString),
+					PermalinkUrl:        make(map[string]sql.NullString),
+					PlaybackCount:       make(map[string]sql.NullInt32),
+					RetrievedUtc:        make(map[string]sql.NullInt32),
+					StreamUrl:           make(map[string]sql.NullString),
+					Streamable:          make(map[string]sql.NullBool),
+					TrackType:           make(map[string]sql.NullString),
+					WaveformUrl:         make(map[string]sql.NullString),
+				}
+
+				// Iterate through each row in db
+				for _, elem := range data {
+
+					key := getValueByColumnNumTrack(groupOne, elem)
+					key += getValueByColumnNumTrack(groupTwo, elem)
+
+					// id
+					id, ok := maps.Id[key]
+
+					if !ok {
+						maps.Id[key] = elem.Id
+					} else {
+						// Since they differ, this is not a valid functional dependency
+						if elem.Id != id {
+
+							isValid[0] = false
+						}
+					}
+
+					// title
+					title, ok := maps.Title[key]
+
+					if !ok {
+						maps.Title[key] = elem.Title
+					} else {
+						// Since they differ, this is not a valid functional dependency
+						if elem.Title.String != title.String {
+
+							isValid[1] = false
+						}
+					}
+
+					// uri
+					uri, ok := maps.Uri[key]
+
+					if !ok {
+						maps.Uri[key] = elem.Uri
+					} else {
+						// Since they differ, this is not a valid functional dependency
+						if elem.Uri.String != uri.String {
+
+							isValid[2] = false
+						}
+					}
+
+					// isrc
+					isrc, ok := maps.Isrc[key]
+
+					if !ok {
+						maps.Isrc[key] = elem.Isrc
+					} else {
+						// Since they differ, this is not a valid functional dependency
+						if elem.Isrc.String != isrc.String {
+
+							isValid[3] = false
+						}
+					}
+
+					// genre
+					genre, ok := maps.Genre[key]
+
+					if !ok {
+						maps.Genre[key] = elem.Genre
+					} else {
+						// Since they differ, this is not a valid functional dependency
+						if elem.Genre.Int32 != genre.Int32 {
+
+							isValid[4] = false
+						}
+					}
+
+					// kind
+					kind, ok := maps.Kind[key]
+
+					if !ok {
+						maps.Kind[key] = elem.Kind
+					} else {
+						// Since they differ, this is not a valid functional dependency
+						if elem.Kind.Int32 != kind.Int32 {
+
+							isValid[5] = false
+						}
+					}
+
+					// license
+					license, ok := maps.License[key]
+
+					if !ok {
+						maps.License[key] = elem.License
+					} else {
+						// Since they differ, this is not a valid functional dependency
+						if elem.License.Int32 != license.Int32 {
+
+							isValid[6] = false
+						}
+					}
+
+					// likesCount
+					likesCount, ok := maps.LikesCount[key]
+
+					if !ok {
+						maps.LikesCount[key] = elem.LikesCount
+					} else {
+						// Since they differ, this is not a valid functional dependency
+						if elem.LikesCount.Int32 != likesCount.Int32 {
+
+							isValid[7] = false
+						}
+					}
+
+					// commentable
+					commentable, ok := maps.Commentable[key]
+
+					if !ok {
+						maps.Commentable[key] = elem.Commentable
+					} else {
+						// Since they differ, this is not a valid functional dependency
+						if elem.Commentable.Bool != commentable.Bool {
+
+							isValid[8] = false
+						}
+					}
+
+					// commentCount
+					commentCount, ok := maps.CommentCount[key]
+
+					if !ok {
+						maps.CommentCount[key] = elem.CommentCount
+					} else {
+						// Since they differ, this is not a valid functional dependency
+						if elem.CommentCount.Int32 != commentCount.Int32 {
+
+							isValid[9] = false
+						}
+					}
+
+					// downloadable
+					downloadable, ok := maps.Downloadable[key]
+
+					if !ok {
+						maps.Downloadable[key] = elem.Downloadable
+					} else {
+						// Since they differ, this is not a valid functional dependency
+						if elem.Downloadable.Bool != downloadable.Bool {
+
+							isValid[10] = false
+						}
+					}
+
+					// downloadCount
+					downloadCount, ok := maps.DownloadCount[key]
+
+					if !ok {
+						maps.DownloadCount[key] = elem.DownloadCount
+					} else {
+						// Since they differ, this is not a valid functional dependency
+						if elem.DownloadCount.Int32 != downloadCount.Int32 {
+
+							isValid[11] = false
+						}
+					}
+
+					// createdAt
+					createdAt, ok := maps.CreatedAt[key]
+
+					if !ok {
+						maps.CreatedAt[key] = elem.CreatedAt
+					} else {
+						// Since they differ, this is not a valid functional dependency
+						if elem.CreatedAt.String != createdAt.String {
+
+							isValid[12] = false
+						}
+					}
+
+					// description
+					description, ok := maps.Description[key]
+
+					if !ok {
+						maps.Description[key] = elem.Description
+					} else {
+						// Since they differ, this is not a valid functional dependency
+						if elem.Description.String != description.String {
+
+							isValid[13] = false
+						}
+					}
+
+					// genre
+					duration, ok := maps.Duration[key]
+
+					if !ok {
+						maps.Duration[key] = elem.Duration
+					} else {
+						// Since they differ, this is not a valid functional dependency
+						if elem.Duration.Int32 != duration.Int32 {
+
+							isValid[14] = false
+						}
+					}
+
+					// labelName
+					labelName, ok := maps.LableName[key]
+
+					if !ok {
+						maps.LableName[key] = elem.LabelName
+					} else {
+						// Since they differ, this is not a valid functional dependency
+						if elem.LabelName.String != labelName.String {
+
+							isValid[15] = false
+						}
+					}
+
+					// lastModified
+					lastModified, ok := maps.LastModified[key]
+
+					if !ok {
+						maps.LastModified[key] = elem.LastModified
+					} else {
+						// Since they differ, this is not a valid functional dependency
+						if elem.LastModified.String != lastModified.String {
+
+							isValid[16] = false
+						}
+					}
+
+					// originalContentSize
+					originalContentSize, ok := maps.OriginalContentSize[key]
+
+					if !ok {
+						maps.OriginalContentSize[key] = elem.OriginalContentSize
+					} else {
+						// Since they differ, this is not a valid functional dependency
+						if elem.OriginalContentSize.Int32 != originalContentSize.Int32 {
+
+							isValid[17] = false
+						}
+					}
+
+					// originalFormat
+					originalFormat, ok := maps.OriginalFormat[key]
+
+					if !ok {
+						maps.OriginalFormat[key] = elem.OriginalFormat
+					} else {
+						// Since they differ, this is not a valid functional dependency
+						if elem.OriginalFormat.String != originalFormat.String {
+
+							isValid[18] = false
+						}
+					}
+
+					// permalink
+					permalink, ok := maps.Permalink[key]
+
+					if !ok {
+						maps.Permalink[key] = elem.Permalink
+					} else {
+						// Since they differ, this is not a valid functional dependency
+						if elem.Permalink.String != permalink.String {
+
+							isValid[19] = false
+						}
+					}
+
+					// permalink
+					permalinkUrl, ok := maps.PermalinkUrl[key]
+
+					if !ok {
+						maps.PermalinkUrl[key] = elem.PermalinkUrl
+					} else {
+						// Since they differ, this is not a valid functional dependency
+						if elem.PermalinkUrl.String != permalinkUrl.String {
+
+							isValid[20] = false
+						}
+					}
+
+					// playbackCount
+					playbackCount, ok := maps.PlaybackCount[key]
+
+					if !ok {
+						maps.PlaybackCount[key] = elem.PlaybackCount
+					} else {
+						// Since they differ, this is not a valid functional dependency
+						if elem.PlaybackCount.Int32 != playbackCount.Int32 {
+
+							isValid[21] = false
+						}
+					}
+
+					// retrievedUTC
+					RetrievedUtc, ok := maps.RetrievedUtc[key]
+
+					if !ok {
+						maps.RetrievedUtc[key] = elem.RetrievedUtc
+					} else {
+						// Since they differ, this is not a valid functional dependency
+						if elem.RetrievedUtc.Int32 != RetrievedUtc.Int32 {
+
+							isValid[22] = false
+						}
+					}
+
+					// streamURL
+					streamURL, ok := maps.StreamUrl[key]
+
+					if !ok {
+						maps.StreamUrl[key] = elem.StreamUrl
+					} else {
+						// Since they differ, this is not a valid functional dependency
+						if elem.StreamUrl.String != streamURL.String {
+
+							isValid[23] = false
+						}
+					}
+
+					// streamable
+					streamable, ok := maps.Streamable[key]
+
+					if !ok {
+						maps.Streamable[key] = elem.Streamable
+					} else {
+						// Since they differ, this is not a valid functional dependency
+						if elem.Streamable.Bool != streamable.Bool {
+
+							isValid[24] = false
+						}
+					}
+
+					// trackType
+					trackType, ok := maps.TrackType[key]
+
+					if !ok {
+						maps.TrackType[key] = elem.TrackType
+					} else {
+						// Since they differ, this is not a valid functional dependency
+						if elem.TrackType.String != trackType.String {
+
+							isValid[25] = false
+						}
+					}
+
+					// waveformURL
+					waveformURL, ok := maps.WaveformUrl[key]
+
+					if !ok {
+						maps.WaveformUrl[key] = elem.WaveformUrl
+					} else {
+						// Since they differ, this is not a valid functional dependency
+						if elem.WaveformUrl.String != waveformURL.String {
+
+							isValid[26] = false
+						}
+					}
+				}
+
+				header := ""
+
+				switch groupOne {
+				case 0:
+					header += "id,"
+				case 1:
+					header += "title,"
+				case 2:
+					header += "uri,"
+				case 3:
+					header += "isrc,"
+				case 4:
+					header += "genre,"
+				case 5:
+					header += "kind,"
+				case 6:
+					header += "license,"
+				case 7:
+					header += "likes_count,"
+				case 8:
+					header += "commentable,"
+				case 9:
+					header += "comment_count,"
+				case 10:
+					header += "downloadable,"
+				case 11:
+					header += "download_count,"
+				case 12:
+					header += "created_at,"
+				case 13:
+					header += "description,"
+				case 14:
+					header += "duration,"
+				case 15:
+					header += "label_name,"
+				case 16:
+					header += "last_modified,"
+				case 17:
+					header += "original_content_size,"
+				case 18:
+					header += "original_format,"
+				case 19:
+					header += "permalink,"
+				case 20:
+					header += "permalink_url,"
+				case 21:
+					header += "playback_count,"
+				case 22:
+					header += "received_utc,"
+				case 23:
+					header += "stream_url,"
+				case 24:
+					header += "streamable,"
+				case 25:
+					header += "track_type,"
+				case 26:
+					header += "waveform_url,"
+				}
+
+				switch groupTwo {
+				case 0:
+					header += "id->"
+				case 1:
+					header += "title->"
+				case 2:
+					header += "uri->"
+				case 3:
+					header += "isrc->"
+				case 4:
+					header += "genre->"
+				case 5:
+					header += "kind->"
+				case 6:
+					header += "license->"
+				case 7:
+					header += "likes_count->"
+				case 8:
+					header += "commentable->"
+				case 9:
+					header += "comment_count->"
+				case 10:
+					header += "downloadable->"
+				case 11:
+					header += "download_count->"
+				case 12:
+					header += "created_at->"
+				case 13:
+					header += "description->"
+				case 14:
+					header += "duration->"
+				case 15:
+					header += "label_name->"
+				case 16:
+					header += "last_modified->"
+				case 17:
+					header += "original_content_size->"
+				case 18:
+					header += "original_format->"
+				case 19:
+					header += "permalink->"
+				case 20:
+					header += "permalink_url->"
+				case 21:
+					header += "playback_count->"
+				case 22:
+					header += "received_utc->"
+				case 23:
+					header += "stream_url->"
+				case 24:
+					header += "streamable->"
+				case 25:
+					header += "track_type->"
+				case 26:
+					header += "waveform_url->"
+				}
+
+				for idx, valid := range isValid {
+					if valid && idx != groupOne && idx != groupTwo &&
+						!(relArr[groupOne][idx] || relArr[groupTwo][idx]) {
+						switch idx {
+						case 0:
+							println(header + "id")
+						case 1:
+							println(header + "title")
+						case 2:
+							println(header + "uri")
+						case 3:
+							println(header + "isrc")
+						case 4:
+							println(header + "genre")
+						case 5:
+							println(header + "kind")
+						case 6:
+							println(header + "license")
+						case 7:
+							println(header + "likes_count")
+						case 8:
+							println(header + "commentable")
+						case 9:
+							println(header + "comment_count")
+						case 10:
+							println(header + "downloadable")
+						case 11:
+							println(header + "download_count")
+						case 12:
+							println(header + "created_at")
+						case 13:
+							println(header + "description")
+						case 14:
+							println(header + "duration")
+						case 15:
+							println(header + "label_name")
+						case 16:
+							println(header + "last_modified")
+						case 17:
+							println(header + "original_content_size")
+						case 18:
+							println(header + "original_format")
+						case 19:
+							println(header + "permalink")
+						case 20:
+							println(header + "permalink_url")
+						case 21:
+							println(header + "playback_count")
+						case 22:
+							println(header + "received_utc")
+						case 23:
+							println(header + "stream_url")
+						case 24:
+							println(header + "streamable")
+						case 25:
+							println(header + "track_type")
+						case 26:
+							println(header + "waveform_url")
+						}
+					}
+				}
+			}
+		}
+	}
+}
 
 // run with go build userFDs.go
 func main() {
@@ -957,8 +1378,8 @@ func main() {
 	// Can't use go routines because I actually need checkGroupsOfOne to finish before checkGroupsOfTwo
 	// I'm going to keep a map of what functional dependencies I got from one and then not print them if that's what
 	// I get from two
-	_ = checkGroupsOfOneTrack(data)
-	//checkGroupOfTwo(data, relArr)
+	relArr := checkGroupsOfOneTrack(data)
+	checkGroupOfTwoTrack(data, relArr)
 
 	t := time.Now()
 	elapsed := t.Sub(start)
